1766ad9f66333f56ee83181065171fb9645cb60e
DevashishBoini <bdevashish957@gmail.com>
2023-12-01 09:00:40 +0530

    fix_reset

--- server/database.js
+++ server/database.js
@@ -1,11 +1,14 @@
 const {Pool} = require('pg/lib')
 
+
 const pool = new Pool({
     host: 'cms.czptthdtnwt9.us-east-2.rds.amazonaws.com',
     port: 5432,
     user: 'postgres',
     password: 'Post_gres1',
     database: 'postgres',
+    connectionLimit: 5,
+    connectionTimeoutMillis: 10000,
     ssl: {
         rejectUnauthorized: false
     }
